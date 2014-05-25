eb69990f6b2cac59820ba6e9fcc5cc0b8446e919
wos <wos@192.168.1.3>
2014-05-25 00:03:15 -0300

    commit criação funcionario

---
+++ src/br/ufg/inf/almoxarifado/model/Funcionario.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ufg.inf.almoxarifado.model;
+
+/**
+ *
+ * @author wos
+ */
+public class Funcionario {
+    private String nome;
+    private String snome;
+    private String fone;
+    private String endereco;
+    private int cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSnome() {
+        return snome;
+    }
+
+    public void setSnome(String snome) {
+        this.snome = snome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+}
