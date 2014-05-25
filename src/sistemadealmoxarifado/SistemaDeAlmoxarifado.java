eea305f6cf2fbec1319813d7fb6e7ccb3dc5bc3c
wos <wos@192.168.1.3>
2014-05-24 23:52:57 -0300

    commit criação dos pacotes 2

--- src/sistemadealmoxarifado/SistemaDeAlmoxarifado.java
+++ src/sistemadealmoxarifado/SistemaDeAlmoxarifado.java
@@ -17,7 +17,7 @@ public class SistemaDeAlmoxarifado {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //commit 4
+        //commit 5
     }
     
 }
