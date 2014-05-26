207b70ee544ddfa568ed88a94b3c19395a5c4a04
wos <wos@192.168.1.3>
2014-05-25 22:32:07 -0300

    commit exception

--- src/br/ufg/inf/almoxarifado/data/DepartamentoDao.java
+++ src/br/ufg/inf/almoxarifado/data/DepartamentoDao.java
@@ -13,6 +13,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -30,14 +34,84 @@ public class DepartamentoDao {
     ResultSet rs;
     Statement st;
         
-    public void salvar(Departamento depto) throws SQLException {
-    conexao=ConnectionFactory.getInstance().getConnection();
+    public void salvar(Departamento depto) {
+        
+    try {
+    conexao=ConnectionFactory.getInstance().getConnection();        
     String sql="INSERT INTO "+TABELA+"("+NOME+","+DESCRICAO+") values(?,?)";
     ps=conexao.prepareStatement(sql);
-    //ps.setInt(1, depto.getCodigo());
+   // ps.setInt(1, depto.getCodigo());
     ps.setString(1, depto.getNome());
     ps.setString(2, depto.getDescricao());
     ps.executeQuery();
+    } 
+    
+    catch (SQLException ex) {
+           throw new RuntimeException("Erro de SQL: "+ex.getLocalizedMessage());
+        }
+    catch(RuntimeException ec) {
+         throw new RuntimeException("Erro em tempo de execução: "+ec.getLocalizedMessage());
+    }
+    }
+    
+    public void atualizar(Departamento depto) throws SQLException {
+    
+        conexao=ConnectionFactory.getInstance().getConnection();
+        String sql="UPDATE "+TABELA+" SET " + NOME + " = ?, " + DESCRICAO + " = ? WHERE " + CODIGO + " = ? ";
+        ps=conexao.prepareStatement(sql);
+        ps.setString(1, depto.getNome());
+        ps.setString(2, depto.getDescricao());
+        ps.setInt(3, depto.getCodigo());
+        ps.executeUpdate();
+     
+    }
+    
+    public void remover(Departamento depto) throws SQLException {
+        
+        conexao=ConnectionFactory.getInstance().getConnection();
+        String sql = "DELETE FROM " + TABELA + " WHERE " + CODIGO + "= ?";
+        ps=conexao.prepareStatement(sql);
+        ps.setInt(1, depto.getCodigo());
+        ps.executeUpdate();
+    }
+    
+    public List<Departamento> listar() throws SQLException {
+        List<Departamento> deptos = new ArrayList<>();
+
+        conexao=ConnectionFactory.getInstance().getConnection();
+        String sql="SELECT * FROM "+TABELA;
+        st=conexao.createStatement();
+        rs = st.executeQuery(sql);
+        
+        while (rs.next()) {
+                Departamento depto = new Departamento();
+                depto.setCodigo(rs.getInt(CODIGO));
+                depto.setNome(rs.getString(NOME));
+                depto.setDescricao(rs.getString(DESCRICAO));
+                deptos.add(depto);
+            }
+        return deptos;
+    }
+    
+    
+    public Departamento getByDepto(String nome) throws SQLException {
+
+        Departamento depto = null;
+        
+        conexao = ConnectionFactory.getInstance().getConnection();
+        String sql = "SELECT * from " + TABELA + " WHERE " + NOME + " = ?";
+
+            ps = conexao.prepareStatement(sql);
+
+            ps.setString(1, nome);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                depto = new Departamento();
+                depto.setCodigo(rs.getInt(CODIGO));
+                depto.setNome(rs.getString(NOME));
+                depto.setDescricao(rs.getString(DESCRICAO));
+            }
+            return depto;
     }
     
 }
--- src/sistemadealmoxarifado/SistemaDeAlmoxarifado.java
+++ src/sistemadealmoxarifado/SistemaDeAlmoxarifado.java
@@ -20,17 +20,32 @@ public class SistemaDeAlmoxarifado {
 
     /**
      * @param args the command line arguments
+     * @throws java.sql.SQLException
      */
     public static void main(String[] args) throws SQLException {
        // ConnectionFactory.getInstance().getConnection();
-        Departamento depto2=new Departamento();
+        Departamento depto4=new Departamento();
         //depto.setCodigo(1);
-        depto2.setDescricao("departamento dE LOGÍSTICA");
-        depto2.setNome("Logística");
+        //depto4.setDescricao("departamento dE Limpeza");
+       // depto4.setNome("Limpeza");
+        //depto3.setCodigo(3);
         
         DepartamentoDao deptoDao=new DepartamentoDao();
-        deptoDao.salvar(depto2);
-        
+        //deptoDao.remover(depto3);
+        /*
+        for (Departamento depto : deptoDao.listar()) {
+            System.out.println(depto.getCodigo()+"/");
+            System.out.println(depto.getNome()+"/");
+            System.out.println(depto.getDescricao());
+        }
+        */
+        try {
+        deptoDao.salvar(depto4);
+        } catch(RuntimeException e) {
+            System.out.println("Desculpe, ocorreu um erro, contate o Administrador \n" +  e.getMessage());
+        }
+       
+        //System.out.println(deptoDao.getByDepto("Compras").getNome());
     }
     
 }
